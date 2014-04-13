92d62acfb83254d709835fa4752adc7d1a28291c
karn09 <karn09@gmail.com>
2014-04-13 16:10:48 -0400

    Prototype Chain pattern

--- javascript.js
+++ javascript.js
@@ -20,11 +20,15 @@ var friend = new Person("Nicholas", 29, "Software Engineer");
 var friend2 = new Person("Greg", 27, "Doctor");
 
 friend.sayName();
+
 */
 //========================================================
-//Constructor and Prototype Pattern combined
+// Constructor and Prototype Pattern combined
 // If both are not combined, then if constructor contains an array (reference value), any changes are reflected in any new constructions
+// not as secure as a durable constructor pattern
+
 /*
+
 function Person(name, age, job){
     this.name = name;
     this.age = age;
@@ -38,7 +42,7 @@ Person.prototype = {
         alert(this.name);
     }
 };
-
+// declare new objects
 var person1 = new Person("Nicholas", 29, "Software Engineer");
 var person2 = new Person("Greg", 27, "Doctor");
 
@@ -48,9 +52,10 @@ alert(person2.friends); //Shelby, Court
 */
 //==============================================================
 
+
 // Durable Constructor Pattern
 // Used in secure environment, instance methods on created object don't refer to THIS and constructor not called using NEW
-
+/*
 function Person(name, age, job){
     //create object to return
     var o = new Object();
@@ -65,3 +70,32 @@ function Person(name, age, job){
 
 var friend = Person("Nicholas", 29, "Software Engineer");
 friend.sayName();
+*/
+//=======================================================================
+
+
+// Inheritance - Prototype Chain pattern
+
+function SuperType(){
+    this.property = true;
+}
+
+SuperType.prototype.getSuperValue = function(){
+    return this.property;
+};
+
+function SubType(){
+    this.subproperty = false;
+}
+
+//Inherit from SuperType
+SubType.prototype = new SuperType();
+
+SubType.prototype.getSubValue = function(){
+    return this.subproperty;
+};
+
+var instance = new SubType();
+alert(instance.getSuperValue()); //true
+
+
