d23b3410b1239b825c3d8f9d32f56fb1729879c0
karn09 <karn09@gmail.com>
2014-04-13 18:47:12 -0400

    prototype chain examples

--- javascript.js
+++ javascript.js
@@ -57,13 +57,11 @@ alert(person2.friends); //Shelby, Court
 
 // Durable Constructor Pattern
 // Used in secure environment, instance methods on created object don't refer to THIS and constructor not called using NEW
-<<<<<<< HEAD
 /*
 function Person(name, age, job){
 =======
 
 function Person(name, age, job) {
->>>>>>> b72abfe9dcc542aa02c5b098c6ac62f94e78b289
     //create object to return
     var o = new Object();
     // optional, define private variables/functions here
@@ -83,7 +81,7 @@ friend.sayName();
 
 
 // Inheritance - Prototype Chain pattern
-
+/*
 function SuperType(){
     this.property = true;
 }
@@ -99,22 +97,255 @@ function SubType(){
 //Inherit from SuperType
 SubType.prototype = new SuperType();
 
+// New Method
 SubType.prototype.getSubValue = function(){
     return this.subproperty;
 };
+// override existing method
+SubType.prototype.getSuperValue = function() {
+    return false;
+};
 
 var instance = new SubType();
-alert(instance.getSuperValue()); //true
-
-
-=======
-//friend.sayName();
+alert(instance.getSuperValue()); //true // "false" after adding overriding existing method
 
 
 alert(instance instanceof Object);      //true
 alert(instance instanceof SuperType);   //true
 alert(instance instanceof SubType);     //true
+*/
+// ==============================================================
+// ==============================================================
 
-// ==============================================================
-// ==============================================================
->>>>>>> b72abfe9dcc542aa02c5b098c6ac62f94e78b289
+// breaking prototype chain
+/*
+function SuperType() {
+    this.property = true;
+}
+
+SuperType.prototype.getSuperValue = function(){
+    return this.property;
+};
+
+function SubType() {
+    this.subproperty = false;
+}
+
+//Inherit from SuperType
+SubType.prototype = new SuperType();
+
+// add new method, overwritting previous line
+// breaks prototype chain since now SubType.prototype is now creating an object literal. creates instance of Object instead of SuperType.
+
+SubType.prototype = {
+    getSubValue : function() {
+        return this.subproperty;
+    },
+
+    someOtherMethod : function() {
+        return false;
+    }
+};
+
+var instance = new SubType();
+alert(instance.getSuperValue());
+*/
+
+
+// Prototype chain showing issues of sharing same properties across all new instances
+/*
+function SuperType () {
+    this.colors = ["red","blue","green"];
+}
+
+function SubType () {
+}
+
+// Inherit from SuperType
+SubType.prototype = new SuperType();
+
+var instance1 = new SubType();
+instance1.colors.push("black");
+alert(instance1.colors); // ["red","blue","green"] + "black"
+
+var instance2 = new SubType();
+alert(instance2.colors); // ["red","blue","green","black"]
+*/
+// Cant push arguments into SuperType without affecting all object instances
+// Constructor stealing is used to solve above inheritance problem
+// using apply() and  call() to execute constructor on new objects
+//
+/*
+function SuperType() {
+    this.colors = ["red","blue","green"];
+}
+
+function SubType() {
+    //inherit from SuperType
+    SuperType.call(this); // using .call() or .apply() allows each instance to have own instance of .colors property
+}
+
+var instance1 = new SubType();
+instance1.colors.push("black");
+alert(instance1.colors); // ["red","blue","green"] + "black"
+
+var instance2 = new SubType();
+
+// constructor stealing will let you pas arguments into SuperType from within SubType
+
+function SuperType(name) {
+    this.name = name;
+}
+
+function SubType() {
+    //inherit from SuperType passing in an argument
+    SuperType.call(this, "Nicholas");
+
+    //instance property
+    this.age = 29;
+}
+
+var instance = new SubType();
+alert(instance.name); // Nicholas
+alert(instance.age); // 29
+*/
+/*
+function SuperType(name) {
+    this.name = name;
+    this.colors = ["red", "blue", "green"];
+}
+
+SuperType.prototype.sayName = function() {
+    alert(this.name);
+};
+
+function SubType(name,age){
+    //inherit properties from SuperType
+    SuperType.call(this,name); //inherit name prop
+    this.age = age; // add age prop
+}
+
+// inherits methods
+SubType.prototype = new SuperType();
+SubType.prototype.sayAge = function() {
+    alert(this.age);
+};
+
+var instance1 = new SubType("Nicholas", 29);
+instance1.colors.push("black");
+alert(instance1.colors); // red blue green black
+instance1.sayName(); // Nicholas
+instance1.sayAge();
+*/
+// if instance2 is created, will hold all initialized values, plus properties as defined
+
+//********************************************************************************************
+//********************************************************************************************
+//********************************************************************************************
+//Prototypal Inheritance
+// Using this pattern, any changes to properties are reflected across all new instances
+/*
+function object(o) {
+    function F() {} // create temp constructor
+    F.prototype = o; // assigns given object as constructor's prototype
+    return new F(); // new instance of temporary type
+}
+
+var person = {
+    name: "Nicholas",
+    friends: ["Shelby", "Court", "Van"]
+};
+
+var anotherPerson = object(person);
+anotherPerson.name = "Greg";
+anotherPerson.friends.push("Rob");
+alert(anotherPerson.friends);// Shelby, Court, Van, Rob
+
+var yetAnotherPerson = object(person);
+yetAnotherPerson.name = "Linda";
+yetAnotherPerson.friends.push("Barbie");
+alert(yetAnotherPerson.friends); // Shelby, Court, Van, Rob, Barbie
+alert(person.friends); // Shelby, Court, Van, Rob, Barbie
+*/
+//ECMAScript5 adds Object.create() to formalize above concept of Prototypal Inheritance
+/*
+var person = {
+    name: "Nicholas",
+    friends: ["Greg","Court","Van"]
+};
+
+var anotherPerson = Object.create(person);
+anotherPerson.name = "Greg";
+anotherPerson.friends.push("Rob");
+
+var yetAnotherPerson = Object.create(person);
+yetAnotherPerson.name = "Linda";
+yetAnotherPerson.friends.push("Barbie");
+
+alert(person.friends);
+
+// Second way to implement prototypal inheritance is to use Object.defineProperties()
+// seems more convoluted to define prototype inheritance this way
+var person = {
+    name: "Nicholas",
+    friends: ["Greg","Court","Van"]
+};
+
+var anotherPerson = Object.create(person, {
+    name: {
+        value: "Greg"
+    }
+});
+alert(anotherPerson.name); // Greg
+
+*/
+
+// Parasitic Combination Inheritance
+// will call functions twice
+// basic pattern -->
+/*
+function object(o) {
+    function F() {}
+    F.prototype = o;
+    return new F();
+}
+
+function inheritPrototype(subType, superType) {
+    var prototype = object(superType.prototype); // create object
+    prototype.constructor = subType; // augment object
+    subType.prototype = prototype; // assign object
+}
+
+function SuperType(name) {
+    this.name = name;
+    this.color = ["red", "blue", "green"];
+}
+
+SuperType.prototype.sayName = function () {
+    alert(this.name);
+};
+
+function SubType(name, age) {
+    SuperType.call(this, name);
+    this.age = age;
+}
+
+inheritPrototype(SubType, SuperType);
+SubType.prototype.sayAge = function () {
+    alert(this.age);
+};
+// ---> end basic pattern
+// declare new properties in instance1/instance2
+var instance1 = new SubType("Nicholas", 29);
+instance1.colors.push("black");
+alert(instance1.colors); //"red,blue,green,black"
+instance1.sayName(); //"Nicholas";
+instance1.sayAge(); //29
+
+
+var instance2 = new SubType("Greg", 27);
+alert(instance2.colors); //"red,blue,green"
+instance2.sayName(); //"Greg";
+instance2.sayAge(); //27
+
+*/
